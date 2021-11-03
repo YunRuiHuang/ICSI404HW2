37d5c07ab37b58a3415abe823d8ce25ed067a4f0
YunRuiHuang <yunruihuang@gmail.com>
2021-11-02 21:16:44 -0400

    first upload to blackboard

--- src/ALU.java
+++ src/ALU.java
@@ -1,34 +1,77 @@
+/**
+ * @author Yunrui Huang
+ * 11/2/2021
+ */
+
 public class ALU {
+
+    /**
+     * four status flags
+     */
     private Boolean ZF; //set true if all bit is 0
     private Boolean NF; //set true if negative
     private Boolean CF; //set true if last bit carry-out is 1
     private Boolean OF; //set true if overflow
 
 
+    /**
+     * The constructor of the ALU class
+     * set all the status flags to false(0)
+     */
     public ALU(){
         this.ZF = false;
         this.NF = false;
         this.CF = false;
         this.OF = false;
-        //LongWord longWord = new LongWord();
     }
 
+    /**
+     * get the status of ZF flag
+     * @return
+     * if true return 1, false return 0
+     */
     public int getZF(){
         return this.ZF?1:0;
     }
 
+    /**
+     * get the status of NF flag
+     * @return
+     * if true return 1, false return 0
+     */
     public int getNF(){
         return this.NF?1:0;
     }
 
+    /**
+     * get the status of CF flag
+     * @return
+     * if true return 1, false return 0
+     */
     public int getCF(){
         return this.CF?1:0;
     }
 
+    /**
+     * get the status of OF flag
+     * @return
+     * if true return 1, false return 0
+     */
     public int getOF(){
         return this.OF?1:0;
     }
 
+    /**
+     * the only method to control the operate
+     * @param code
+     * the code of what operate to do base on the table
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     public LongWord operate(int code, LongWord op1, LongWord op2){
         if(code == 0){
             return and(op1,op2);
@@ -51,6 +94,17 @@ public class ALU {
         }
     }
 
+    /**
+     * to add or subtraction two LongWord, dependent on cin
+     * @param a
+     * the first LongWord to be add or subtraction
+     * @param holderb
+     * the second LongWord to add or to subtraction to first LongWord
+     * @param cin
+     * If cin as true then subtraction, false then add
+     * @return
+     * the LongWord after add or subtraction
+     */
     private LongWord rippleCarryAdd(LongWord a, LongWord holderb, Boolean cin){
         LongWord longWord = new LongWord();
 
@@ -109,6 +163,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * And two LongWord, return a new LongWord
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     private LongWord and(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -120,6 +183,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Or two LongWord, return a new LongWord
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     private LongWord or(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -131,6 +203,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Xor two LongWord, return a new LongWord
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     private LongWord xor(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -142,6 +223,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Add two LongWord, return a new LongWord
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     private LongWord add(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord = rippleCarryAdd(op1,op2,false);
@@ -150,6 +240,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Subtract two LongWord, return a new LongWord
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the second LongWord to operate
+     * @return
+     * the LongWord after operate
+     */
     private LongWord sub(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord = rippleCarryAdd(op1,op2,true);
@@ -158,6 +257,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * left-shift this LongWord by amount bit (padding with 0's)
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the amount of bit need to shift, in signed
+     * @return
+     * the LongWord after operate
+     */
     private LongWord sll(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -177,6 +285,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Right-shift this LongWord by amount bits (padding with 0')
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the amount of bit need to shift, in signed
+     * @return
+     * the LongWord after operate
+     */
     private LongWord srl(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -188,6 +305,15 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * Right-shift this LongWord by amount bits (sign-extending)
+     * @param op1
+     * the first LongWord to operate
+     * @param op2
+     * the amount of bit need to shift, in signed
+     * @return
+     * the LongWord after operate
+     */
     private LongWord sra(LongWord op1, LongWord op2){
         LongWord longWord = new LongWord();
         longWord.copy(op1);
@@ -199,6 +325,11 @@ public class ALU {
         return longWord;
     }
 
+    /**
+     * override the toString method to print all four status flags
+     * @return
+     * A string with all four status flags
+     */
     public String toString(){
         return "ZF: " + getZF() + "  NF: " + getNF() + "  CF: " + getCF() + "  OF: " + getOF();
     }
--- src/ALUTester.java
+++ src/ALUTester.java
@@ -1,3 +1,8 @@
+/**
+ * @author Yunrui Huang
+ * 11/2/2021
+ */
+
 public class ALUTester {
     public ALUTester(){
 
@@ -12,27 +17,37 @@ public class ALUTester {
         System.out.println(longWord1.toString());
         System.out.println(longWord2.toString());
 
-        //testing add
+        /**
+         * testing add
+         */
         longWord3 = alu.operate(3,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
         //System.out.println("2: " + longWord2.toString());
 
-        //testing sub
+        /**
+         * testing sub
+         */
         longWord3 = alu.operate(4,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing carry out flag
+        /**
+         * testing carry out flag
+         */
         //System.out.println("3: " + longWord2.toString());
         longWord1 = alu.operate(3,longWord3,longWord2);
         System.out.println(longWord1.toString() + " " + alu.toString());
 
-        //testing overflow flag
+        /**
+         * testing overflow flag
+         */
         longWord2.set(-1);
         longWord3.set(-2147483648);
         longWord3 = alu.operate(3,longWord3, longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing and
+        /**
+         * testing and
+         */
         longWord1.set(10);
         longWord2.set(1000);
         System.out.printf("\n\ntesting and\n");
@@ -41,7 +56,9 @@ public class ALUTester {
         longWord3 = alu.operate(0,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing or
+        /**
+         * testing or
+         */
         longWord1.set(10);
         longWord2.set(1000);
         System.out.printf("\n\ntesting or\n");
@@ -50,7 +67,9 @@ public class ALUTester {
         longWord3 = alu.operate(1,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing xor
+        /**
+         * testing xor
+         */
         longWord1.set(10);
         longWord2.set(1000);
         System.out.printf("\n\ntesting xor\n");
@@ -59,7 +78,9 @@ public class ALUTester {
         longWord3 = alu.operate(2,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing Not
+        /**
+         * testing Not
+         */
         longWord1.set(10);
         longWord2.set(-1);
         System.out.printf("\n\ntesting not\n");
@@ -68,7 +89,9 @@ public class ALUTester {
         longWord3 = alu.operate(2,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing SLL
+        /**
+         * testing SLL
+         */
         longWord1.set(-1);
         longWord1.clearBit(30);
         longWord2.set(1);
@@ -78,7 +101,9 @@ public class ALUTester {
         longWord3 = alu.operate(5,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing SRL
+        /**
+         * testing SRL
+         */
         longWord1.set(1000);
         longWord2.set(2);
         System.out.printf("\n\ntesting srl\n");
@@ -87,7 +112,9 @@ public class ALUTester {
         longWord3 = alu.operate(6,longWord1,longWord2);
         System.out.println(longWord3.toString() + " " + alu.toString());
 
-        //testing SRA
+        /**
+         * testing SRA
+         */
         longWord1.set(-1000);
         longWord2.set(3);
         System.out.printf("\n\ntesting sra\n");
